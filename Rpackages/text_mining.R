4be78b88f1060629812f571b991a34eea775aa6b
Andrea Fernandez <animalsalvaje@gmail.com>
2015-10-15 16:03:20 -0500

    packages for text mining plus prerequisites in bash

---
+++ Rpackages/text_mining.R
@@ -0,0 +1,3 @@
+
+# GSL needs to be installed (it is done in i_bash_tools.sh)
+pacman::p_load(tm, topicmodels, LDAvis, syzuhet)
--- i_bash_tools.sh
+++ i_bash_tools.sh
@@ -9,3 +9,8 @@ git clone https://github.com/dbro/csvquote.git
 cd csvquote
 make
 sudo make install
+
+# Install GNU scientific library - Need it for libraries for topic modeling in R
+
+echo Installing GNU scientific library -  GSL
+sudo apt-get install -y libgsl0ldbl libgsl0-dev
